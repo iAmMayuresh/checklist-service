04fa350167458a0c7003788218615656046c547c
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-08-22 13:04:20 +0530

    Add files via upload

--- src/main/java/com/audit/auditchecklistservice/feignclient/AuthClient.java
+++ src/main/java/com/audit/auditchecklistservice/feignclient/AuthClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 //@FeignClient(name = "auth-service", url = "$(AUTH_SERVICE_URI:http://localhost:8000)")
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service", url = "http://localhost:8000")
 public interface AuthClient {
 
 	@GetMapping(value = "/validate")
