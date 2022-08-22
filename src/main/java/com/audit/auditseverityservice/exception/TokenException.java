f00a3e4dec8c4e31876dd0967c725644f0095e3d
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-08-22 14:27:29 +0530

    Delete src directory

--- src/main/java/com/audit/auditseverityservice/exception/TokenException.java
+++
@@ -1,9 +0,0 @@
-package com.audit.auditseverityservice.exception;
-
-public class TokenException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public TokenException(String message) {
-		super(message);
-	}
-}
